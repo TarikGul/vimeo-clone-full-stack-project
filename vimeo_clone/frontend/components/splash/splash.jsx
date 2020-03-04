f475e5c5e55ce71b81a2687109c620b50e938a01
Tarik <tariksnow37@gmail.com>
2020-03-03 17:29:47 -0800

    improved css and html

--- vimeo_clone/frontend/components/splash/splash.jsx
+++ vimeo_clone/frontend/components/splash/splash.jsx
@@ -11,21 +11,75 @@ const Splash = () => {
                 </div>
             </div>
             <div className="demo-user-button-container"><button className="demo-user-button">Demo User</button></div>
-            <div className="image-1-container">
+            <div className="image-container">
                 <div className="image-1-left-column">
                     <div className="image-1-column-header">
                         Your video <br/> starts here.
                     </div>
-                    <div className="image-1-column-header">
+                    <div className="image-1-column-description">
                         Creat standout social videso for your business <br/> instantly.
                     </div>
-                    <div>
-                        <button>
-                            try vimeo create
-                        </button>
+                    <div className='image-1-button-div'>
+                        <button className='image-1-button'> Try Vimeo Create </button>
+                    </div>
+                    <div className="image-1-column-description">
+                        Upload your latest cut in the highest codec <br/> possible
+                    </div>
+                    <div className='image-1-button-div'>
+                        <button className='image-1-button'> Start uploading </button>
+                    </div>
+                    <div className="image-1-column-description">
+                        Hir a pro to give your video a world-class look.
+                    </div>
+                    <div className='image-1-button-div'>
+                        <button className='image-1-button'> Post a job </button>
                     </div>
                 </div>
                 <img className='image-1'src="assets/947.png" alt=""/>
+            </div>
+            <div className="image-2-container">
+                <img className='image-2' src="assets/948.jpg" alt="" />
+                <div className="image-2-left-column">
+                    <div className="image-1-column-header">
+                        Publish anywhere, <br /> grow everywhere.
+                    </div>
+                    <div className="image-1-column-description">
+                        publish videos to any channel, customize <br /> 
+                        the player and add powerful marketing tools <br/>
+                        to bump up engagement.
+                    </div>
+                    <div className='image-1-button-div'>
+                        <button className='image-1-button'> Grow your audience </button>
+                    </div>
+                    <div className="image-1-column-description">
+                        See how your videos are performing with <br/>
+                        advanced analytics.
+                    </div>
+                    <div className='image-1-button-div'>
+                        <button className='image-1-button'> Start uploading </button>
+                    </div>
+                </div>
+            </div>
+            <div className="image-container">
+                <div className="image-3-left-column">
+                    <div className="image-1-column-header">
+                        Simple, creative <br /> collaboration.
+                    </div>
+                    <div className="image-1-column-description">
+                        Stremline feedback with time-coded comments, and <br />
+                        assemble your team on your terms.
+                    </div>
+                    <div className='image-1-button-div'>
+                        <button className='image-1-button'> Start collaborating </button>
+                    </div>
+                    <div className="image-1-column-description">
+                        Share securely with comprehensive privacy options.
+                    </div>
+                    <div className='image-1-button-div'>
+                        <button className='image-1-button'> See privacy options </button>
+                    </div>
+                </div>
+                <img className='image-3' src="assets/953.jpg" alt="" />
             </div>
         </div>
     )
