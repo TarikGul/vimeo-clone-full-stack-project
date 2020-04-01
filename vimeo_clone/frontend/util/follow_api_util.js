75892761846f138f6a35f92da03d3476aee7bd63
Tarik <tariksnow37@gmail.com>
2020-03-31 22:07:11 -0700

    add follower util

---
+++ vimeo_clone/frontend/util/follow_api_util.js
@@ -0,0 +1,14 @@
+export const createFollow = follow => {
+    return $.ajax({
+        url: `api/followers`,
+        method: 'POST',
+        data: { follow }
+    })
+}
+
+export const deleteFollow = id => {
+    return $.ajax({
+        url: `api/followers/${id}`,
+        method: 'DELETE'
+    })
+}
