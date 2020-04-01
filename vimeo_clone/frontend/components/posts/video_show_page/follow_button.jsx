8e9d9d0077ec7b888bd9db1699bf4fa4e3f17d48
Tarik <tariksnow37@gmail.com>
2020-04-01 15:32:54 -0700

    use hook to change follow button on hover

--- vimeo_clone/frontend/components/posts/video_show_page/follow_button.jsx
+++ vimeo_clone/frontend/components/posts/video_show_page/follow_button.jsx
@@ -1,6 +1,8 @@
+import { useRef, useState, useEffect } from 'react';
 import React from 'react'
 
 const FollowButton = props => {
+    const [hoverRef, isHovered] = useHover();
     const { sessionId, postId, entities } = props;
     const isFollowing = () => {
         const curPostUsername = entities.posts[postId].uploader.username;
@@ -15,17 +17,30 @@ const FollowButton = props => {
     const following = isFollowing();
     return (
         
-        <div className="Follow-button-container">
+        <div className="Follow-button-container" ref={hoverRef}>
             {
                 following 
                 ?
-                (
-                    <button className="following-button">
-                        <img className="following-i" src="/follow-check.svg" width="14" height="14" />
-                        <div className="following-button-text">
-                            Following
-                        </div>
-                    </button>
+                (   
+                        isHovered 
+                        ?
+                        (
+                            <button className="unfollowing-button" >
+                                <img className="unfollowing-i" src="/unfollow-x.svg" width="8" height="8" />
+                                <div className="unfollowing-button-text">
+                                    UnFollow
+                                </div>
+                            </button>
+                        ) : (
+                            <button className="following-button" >
+                                <img className="following-i" src="/follow-check.svg" width="14" height="14" />
+                                <div className="following-button-text">
+                                    Following
+                                </div>
+                            </button>
+                        )
+                    
+                    
                 ) : (
                     <button className="follow-button">
                         <img className="follow-i" src="/follow-plus.svg" width="14" height="14" />
@@ -37,6 +52,33 @@ const FollowButton = props => {
             }
         </div>
     )
+}
+
+function useHover() {
+    const [value, setValue] = useState(false);
+
+    const ref = useRef(null);
+
+    const handleMouseOver = () => setValue(true);
+    const handleMouseOut = () => setValue(false);
+
+    useEffect(
+        () => {
+            const node = ref.current;
+            if (node) {
+                node.addEventListener('mouseover', handleMouseOver);
+                node.addEventListener('mouseout', handleMouseOut);
+
+                return () => {
+                    node.removeEventListener('mouseover', handleMouseOver);
+                    node.removeEventListener('mouseout', handleMouseOut);
+                };
+            }
+        },
+        [ref.current] // Recall only if ref changes
+    );
+
+    return [ref, value];
 }
 
 export default FollowButton;
