39ea8c6b38ab55f5d6ded716e1915ada1db35b6d
Tarik <tariksnow37@gmail.com>
2020-04-04 04:45:55 -0700

    add date to recent post

--- vimeo_clone/frontend/components/posts/user_recent_posts/post_user_item.jsx
+++ vimeo_clone/frontend/components/posts/user_recent_posts/post_user_item.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { timeSince } from '../../../util/date_api_util'
 
 const PostUserItem = props => {
-
+    const { post } = props
+    const date = new Date(post.created_at)
     return (
         <div className="recent-post-box-container">
             <div className="recent-post-box">
-                <img className="recent-thumbnail" src={props.post.thumbnailUrl} alt=""/>
+                <img className="recent-thumbnail" src={post.thumbnailUrl} alt=""/>
+                <div className="recent-post-title">
+                    {post.title}
+                </div>
+                <div className="recent-date-posted">
+                    {`${timeSince(date)} ago`}
+                </div>
             </div>
         </div>
     )
