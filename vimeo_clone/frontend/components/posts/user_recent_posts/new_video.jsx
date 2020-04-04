0d287e5362e048af36cd7f66c290532126cb8d0e
Tarik <tariksnow37@gmail.com>
2020-04-04 02:51:36 -0700

    got svg working

---
+++ vimeo_clone/frontend/components/posts/user_recent_posts/new_video.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import useHover from '../../../hooks/use_hover';
+
+const NewVideoBox = props => {
+    const [hoverRef, isHovered] = useHover();
+
+    return (
+        <div className="new-video-box-container" ref={hoverRef}>
+            {
+                isHovered 
+                ?
+                (
+                    <div className="new-video-box">
+                        <img className="new-video-i-blue" src="/new-video-plus.svg" width="30" height="30" />
+                        <div className="new-video-box-text">
+                                New video
+                        </div>
+                    </div>
+                    
+                ) : (
+                    <div className="new-video-box">
+                        <img className="new-video-i" src="/new-video-plus.svg" width="30" height="30" />
+                        <div className="new-video-box-text">
+                            New video
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
+}
+
+export default NewVideoBox;
--- vimeo_clone/frontend/components/posts/user_recent_posts/user_recent_posts.jsx
+++ vimeo_clone/frontend/components/posts/user_recent_posts/user_recent_posts.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NewVideoBox from './new_video'
 
 class UserRecentPosts extends React.Component {
     constructor(props) {
@@ -8,17 +9,14 @@ class UserRecentPosts extends React.Component {
     
     render() {
         return (
-            <div className="recent-posts-container">
+            <div className="recent-posts-main-container">
                 <div className="recent-videos-title-container">
                     <div className="recent-videos-title">
                         Recent Videos
                     </div>
                 </div>
-                <div className="new-video-box">
-                    <img src="/new-video-plus.svg" width="30" height="30"/>
-                    <div class="new-video-box-text">
-                        New video
-                    </div>
+                <div className="recent-posts-container">
+                    <NewVideoBox />
                 </div>
             </div>
         )
