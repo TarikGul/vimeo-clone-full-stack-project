57b5ca92b30e189dee7337df8b7952c1cb07266a
Tarik <tariksnow37@gmail.com>
2020-04-05 04:22:07 -0700

    added videos to preview

--- vimeo_clone/frontend/components/manage/upload/post_form.jsx
+++ vimeo_clone/frontend/components/manage/upload/post_form.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PreviewPost from './preview'
 
 class PostForm extends React.Component {
     constructor(props) {
@@ -10,6 +11,8 @@ class PostForm extends React.Component {
             thumbnailFile: null,
             description: '',
             userId: null,
+            thumbnailUrl: null,
+            videoUrl: null,
             passwordProtected: false
         }
 
@@ -28,9 +31,9 @@ class PostForm extends React.Component {
             const fileReader = new FileReader();
             fileReader.onloadend = () => {
                 if (field === 'thumbnail') {
-                    this.setState({ thumbnailFile: file});
+                    this.setState({ thumbnailFile: file, thumbnailUrl: fileReader.result});
                 } else {
-                    this.setState({ videoFile: file});
+                    this.setState({ videoFile: file, videoUrl: fileReader.result});
                 }
             };
             if (file) {
@@ -45,10 +48,10 @@ class PostForm extends React.Component {
         formData.append('post[title]', this.state.title)
         formData.append('post[description]', this.state.description)
         if (this.state.videoFile) {
-            formData.append('post[video_file]', this.state.videoFile)
+            formData.append('post[video]', this.state.videoFile)
         }
         if (this.state.thumbFile) {
-            formData.append('post[thumbnail_file]', this.state.thumbnailFile)
+            formData.append('post[thumbnail]', this.state.thumbnailFile)
         }
         this.props.createPost(formData)
             .then((response) => {
@@ -58,46 +61,58 @@ class PostForm extends React.Component {
 
     
     render() {
+        const { thumbnailUrl, videoUrl } = this.state
         return (
             <div className="upload-form-container">
                 <div className="upload-form-inner-container">
                     <form className="upload-form" onSubmit={this.handleSubmit}>
-                        <div className="upload-video-thumbnail-container">
-                            <img src="/upload-video.svg" alt="" width="85" height="56"/>
-                            <div className="upload-box-text">
-                                Upload any video that is an <br/>
-                                .mp4
+                        {
+                            (thumbnailUrl && videoUrl) 
+                            ?
+                            (
+                            <PreviewPost
+                                thumbnailUrl={thumbnailUrl}
+                                videoUrl={videoUrl}
+                                update={this.update} />
+                            ) : (
+                            <div className="upload-video-thumbnail-container">
+                                <img src="/upload-video.svg" alt="" width="85" height="56" />
+                                <div className="upload-box-text">
+                                    Upload any video that is an <br />
+                                    .mp4
+                                </div>
+                                <button className="upload-button">
+                                    <img className="cloud-i" src="/cloud-upload.svg" width="18" height="18" />
+                                    {/* Change this to a loading bar */}
+                                    <div className="upload-button-text">
+                                        {!this.state.videoFile ?
+                                            `Choose video files` : ` Upload Successful`
+                                        }
+                                    </div>
+                                    <input type="file"
+                                        accept="video/mp4"
+                                        onChange={this.handleFile('video')}
+                                        className="upload-file-input" />
+                                </button>
+                                <button className="upload-button">
+                                    <img className="cloud-i" src="/cloud-upload.svg" width="18" height="18" />
+                                    {/* Change this to a loading bar */}
+                                    <div className="upload-button-text">
+                                        {!this.state.thumbnailFile ?
+                                            `Choose thumbnail files` : ` Upload Successful`
+                                        }
+                                    </div>
+                                    <input type="file"
+                                        accept="image/png,image/jpeg"
+                                        onChange={this.handleFile('thumbnail')}
+                                        className="upload-file-input" />
+                                </button>
+                                <div className="preview-message">
+                                    Once files are uploaded, fill out the preview page before submitting
+                                </div>
                             </div>
-                            <button className="upload-button"> 
-                                <img className="cloud-i" src="/cloud-upload.svg" width="18" height="18"/> 
-                                {/* Change this to a loading bar */}
-                                <div className="upload-button-text">
-                                    {!this.state.videoFile ?
-                                        `Choose video files` : ` Upload Successful`
-                                    }
-                                </div>
-                                <input type="file"
-                                       accept="video/mp4"
-                                       onChange={this.handleFile('video')}
-                                       className="upload-file-input"/>
-                            </button>
-                            <button className="upload-button" for="file">
-                                <img className="cloud-i" src="/cloud-upload.svg" width="18" height="18" />
-                                {/* Change this to a loading bar */}
-                                <div className="upload-button-text">
-                                    {!this.state.thumbnailFile ?
-                                        `Choose thumbnail files` : ` Upload Successful`
-                                    }
-                                </div>
-                                <input type="file"
-                                       accept="image/png,image/jpeg"
-                                       onChange={this.handleFile('video')}
-                                       className="upload-file-input" />
-                            </button>
-                            <div className="preview-message">
-                                Once files are uploaded, fill out the preview page before submitting
-                            </div>  
-                        </div>
+                            )
+                        }
                         
                     </form>
                 </div>
--- vimeo_clone/frontend/components/manage/upload/preview.jsx
+++ vimeo_clone/frontend/components/manage/upload/preview.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import ReactPlayer from 'react-player'
 
 const PreviewPost = props => {
-
+    const { update, videoUrl } = props
+    debugger
     return (
-        <div>
-
+        <div className="preview-container">
+            <div className="video-preview-container">
+                <ReactPlayer url={videoUrl} 
+                             width="90%" 
+                             height="90%" 
+                             controls/>
+            </div>
+            <div className="title-description-container">
+                <label>Title</label>
+                <input type="text"
+                    onChange={update('title')}/>
+                <label>Description</label>
+                <input type="textarea"
+                    onChange={update('description')}/>
+            </div>
         </div>
     )
 }
+
+export default PreviewPost;
