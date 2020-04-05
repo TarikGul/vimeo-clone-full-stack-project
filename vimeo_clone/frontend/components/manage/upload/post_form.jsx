d238bb549c0542f737b3da6f30a254c83e83c2f1
Tarik <tariksnow37@gmail.com>
2020-04-05 02:12:44 -0700

    lgtm

--- vimeo_clone/frontend/components/manage/upload/post_form.jsx
+++ vimeo_clone/frontend/components/manage/upload/post_form.jsx
@@ -8,14 +8,61 @@ class PostForm extends React.Component {
             title: '',
             videoFile: null,
             thumbnailFile: null,
-            description: null,
+            description: '',
+            userId: null,
+            passwordProtected: false
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+    update(field) {
+        return (e) => this.setState({ [field]: e.currentTarget.value })
+    }
+
+    
+
+    handleSubmit() {
+
+    }
+
+    
 
     render() {
         return (
-            <div>
+            <div className="upload-form-container">
+                <div className="upload-form-inner-container">
+                    <form className="upload-form" onSubmit={this.handleSubmit}>
+                        <div className="upload-video-thumbnail-container">
+                            <img src="/upload-video.svg" alt="" width="85" height="56"/>
+                            <div className="upload-box-text">
+                                Upload any video that is an <br/>
+                                .mp4
+                            </div>
+                            <button className="upload-button"> 
+                                <img className="cloud-i" src="/cloud-upload.svg" width="18" height="18"/> 
+                                {/* Change this to a loading bar */}
+                                <div className="upload-button-text">
+                                    {!this.state.videoFile ?
+                                        `Choose video files` : ` Upload Successful`
+                                    }
+                                </div>
+                            </button>
+                            <button className="upload-button">
+                                <img className="cloud-i" src="/cloud-upload.svg" width="18" height="18" />
+                                {/* Change this to a loading bar */}
+                                <div className="upload-button-text">
+                                    {!this.state.thumbnailFile ?
+                                        `Choose thumbnail files` : ` Upload Successful`
+                                    }
+                                </div>
+                            </button>
+                        </div>
+                        <div className="title-description-container">
 
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
