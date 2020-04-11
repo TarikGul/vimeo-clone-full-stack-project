c19ca6cb84e12fa132d2c64cf4ae5f554c83d67f
Tarik <tariksnow37@gmail.com>
2020-04-10 17:27:08 -0700

    add boilerplate for results page

---
+++ vimeo_clone/frontend/components/search/results.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ResultsPage extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default ResultsPage;
---
+++ vimeo_clone/frontend/components/search/results_container.js
@@ -0,0 +1,22 @@
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import ResultsPage from './results';
+
+const msp = state => {
+    const { ui, entities } = state
+    return {
+        ui,
+        entities
+    };
+};
+
+const mdtp = dispatch => {
+    return {
+
+    };
+};
+
+export default connect(
+    msp, 
+    mdtp
+)(ResultsPage)
