cc97b46e6e9052d4aca1322a5dcc5e0f67ebc18b
Tarik <tariksnow37@gmail.com>
2020-04-14 13:10:03 -0700

    cleanup code in follows

--- vimeo_clone/frontend/components/follows/follow_button.jsx
+++ vimeo_clone/frontend/components/follows/follow_button.jsx
@@ -30,7 +30,7 @@ const FollowButton = props => {
     const leaderId = isFollowing('leaderId');
     const uploaderId = entities.posts[postId].uploader.id;
     return (
-        <div className="Follow-button-container" ref={hoverRef}>
+        <div className='Follow-button-container' ref={hoverRef}>
             {
                 isFollowing('following') 
                 ?
@@ -38,16 +38,16 @@ const FollowButton = props => {
                         isHovered 
                         ?
                         (
-                            <button className="unfollowing-button" onClick={() => deleteFollow(leaderId)}>
-                                <img className="unfollowing-i" src="/unfollow-x.svg" width="8" height="8" />
-                                <div className="unfollowing-button-text">
+                            <button className='unfollowing-button' onClick={() => deleteFollow(leaderId)}>
+                                <img className='unfollowing-i' src='/unfollow-x.svg' width='8' height='8' />
+                                <div className='unfollowing-button-text'>
                                     Unfollow
                                 </div>
                             </button>
                         ) : (
-                            <button className="following-button">
-                                <img className="following-i" src="/follow-check.svg" width="14" height="14" />
-                                <div className="following-button-text">
+                            <button className='following-button'>
+                                <img className='following-i' src='/follow-check.svg' width='14' height='14' />
+                                <div className='following-button-text'>
                                     Following
                                 </div>
                             </button>
@@ -55,9 +55,9 @@ const FollowButton = props => {
                     
                     
                 ) : (
-                        <button className="follow-button" onClick={() => createFollow({ user_id: sessionId, follower_id: uploaderId })}>
-                        <img className="follow-i" src="/follow-plus.svg" width="14" height="14" />
-                        <div className="follow-button-text">
+                        <button className='follow-button' onClick={() => createFollow({ user_id: sessionId, follower_id: uploaderId })}>
+                        <img className='follow-i' src='/follow-plus.svg' width='14' height='14' />
+                        <div className='follow-button-text'>
                             Follow
                         </div>
                     </button> 
