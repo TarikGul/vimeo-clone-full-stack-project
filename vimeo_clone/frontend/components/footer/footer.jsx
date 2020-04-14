67cfe86ac0e8510b0cf0660048e9d5692c85b050
Tarik <tariksnow37@gmail.com>
2020-04-14 13:11:58 -0700

    cleanup code in footer

--- vimeo_clone/frontend/components/footer/footer.jsx
+++ vimeo_clone/frontend/components/footer/footer.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 const Footer = props => {
     return (
-        <div className="footer-container">
-            <div className="footer-top-container">
-                <div className="footer-left-container">
-                    <ul className="list-container-footer">
+        <div className='footer-container'>
+            <div className='footer-top-container'>
+                <div className='footer-left-container'>
+                    <ul className='list-container-footer'>
                         <li>
-                            <a className='github-link atag' href="https://github.com/TarikGul">Github</a>
+                            <a className='github-link atag' href='https://github.com/TarikGul'>Github</a>
                         </li>
                         <li>
-                            <a className="dreams-tag atag" href="https://www.youtube.com/watch?v=8WsHwXs_aq4">Dreams</a>
+                            <a className='dreams-tag atag' href='https://www.youtube.com/watch?v=8WsHwXs_aq4'>Dreams</a>
                         </li>
                     </ul>
                 </div>
-                <div className="footer-right-container">
+                <div className='footer-right-container'>
                     May the force be with you
                 </div>
             </div>
-            <div className="footer-bottom-container">
-                <div className="footer-bottom-inner-container">
+            <div className='footer-bottom-container'>
+                <div className='footer-bottom-inner-container'>
                     <div className='one-love-sf'>
                     Made with love in SF
                     </div>
