dc5b0c1242556b999f5da480c505029ee83d1420
Tarik <tariksnow37@gmail.com>
2020-04-14 12:47:26 -0700

    fix quotes and cleanup code

--- vimeo_clone/frontend/components/search/result_item.jsx
+++ vimeo_clone/frontend/components/search/result_item.jsx
@@ -4,21 +4,21 @@ import { Link } from 'react-router-dom'
 const ResultItem = props => {
     const { post } = props;
     return (
-        <div className="result-box-container">
-            <Link to={`/posts/${post.id}`} className="result-box-top">
+        <div className='result-box-container'>
+            <Link to={`/posts/${post.id}`} className='result-box-top'>
                 <img 
-                    className="search-thumbnail"
+                    className='search-thumbnail'
                     src={props.post.thumbnailUrl} /> 
             </Link>
-            <div className="result-box-bottom">
-                <Link to={`/posts/${post.id}`} className="result-bottom-title">
+            <div className='result-box-bottom'>
+                <Link to={`/posts/${post.id}`} className='result-bottom-title'>
                     {props.post.title}  
                 </Link>
-                <div className="plays-user-result-box">
-                    <div className="result-bottom-user">
+                <div className='plays-user-result-box'>
+                    <div className='result-bottom-user'>
                         {props.post.uploader.username}
                     </div>
-                    <div className="result-bottom-plays">
+                    <div className='result-bottom-plays'>
                         {`${props.post.plays} views`}
                     </div>
                 </div>
