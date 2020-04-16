4a9e58c9657bd4962fe701d20e1def607558d2fc
Tarik <tariksnow37@gmail.com>
2020-04-16 00:28:49 -0700

    add state to toggle

--- vimeo_clone/frontend/components/posts/video_show_page/toggle.jsx
+++ vimeo_clone/frontend/components/posts/video_show_page/toggle.jsx
@@ -4,11 +4,21 @@ class ToggleButton extends React.Component {
     constructor(props) {
         super(props)
 
+        this.state = {
+            clicked: false
+        }
+
+        this.consoleResult = this.consoleResult.bind(this)
+    }
+
+    consoleResult(e) {
+        e.preventDefault();
+        this.setState(prevState => { clicked: !prevState })
     }
 
     render() {
         return (
-            <div className="toggle-autplay-container">
+            <div className="toggle-autplay-container" onClick={this.consoleResult}>
                 <label className="switch">
                     <input type="checkbox" />
                     <span className="slider round"></span>
