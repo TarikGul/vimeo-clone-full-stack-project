1245d3838685cb7409b305134157ad841d80f7d8
Tarik <tariksnow37@gmail.com>
2020-04-16 17:15:49 -0700

    organize error

--- vimeo_clone/frontend/reducers/errors_reducer.js
+++ vimeo_clone/frontend/reducers/errors_reducer.js
@@ -2,6 +2,8 @@ import { combineReducers } from 'redux';
 
 import sessionErrorsReducer from './session_errors_reducer'
 
-const errorsReducer = combineReducers({ session: sessionErrorsReducer })
+const errorsReducer = combineReducers({ 
+    session: sessionErrorsReducer
+ })
 
 export default errorsReducer;
