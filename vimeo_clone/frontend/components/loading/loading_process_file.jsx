0831dbe90bbe70e7f043437d71d8369254f24611
Tarik <tariksnow37@gmail.com>
2020-04-16 19:26:09 -0700

    add processing

--- vimeo_clone/frontend/components/loading/loading_process_file.jsx
+++ vimeo_clone/frontend/components/loading/loading_process_file.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const processSeconds = (counter) => {
+    const processes = [
+        'Processing...',
+        'Processing.  ',
+        'Processing.. '
+    ];
+    let count = counter % 3;
+
+    return processes[count]
+}
 
 const LoadingProcessFile = props => {
-    
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCounter(counter => counter + 1);
+        }, 300);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="loading-container">
             <div className="loader">
     
             </div>
-            <div className="processing">
-                Processing..
+            <div className="processing" id="processing">
+                {processSeconds(counter)}
             </div>  
         </div>
     )
