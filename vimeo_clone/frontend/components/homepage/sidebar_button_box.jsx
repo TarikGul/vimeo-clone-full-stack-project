0cd64cfb6808660b0a0a39671438ec3c96f22623
Tarik <tariksnow37@gmail.com>
2020-04-17 00:43:49 -0700

    finish link box

--- vimeo_clone/frontend/components/homepage/sidebar_button_box.jsx
+++ vimeo_clone/frontend/components/homepage/sidebar_button_box.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import SidebarButton from './sidebar_button'
 import AvailableStorageContainer from './available_storage_container';
 
@@ -7,10 +8,15 @@ class SidebarButtonBox extends React.Component {
         super(props)
 
         this.reRoute = this.reRoute.bind(this)
+        this.routePortfolio = this.routePortfolio.bind(this)
     }
 
-    componenDidMount() {
-        
+    routePortfolio(field) {
+        if(field === 'github') {
+            window.location.href = 'https://github.com/TarikGul'
+        } else if(field === 'linkedin') {
+            window.location.href = 'https://www.linkedin.com/in/tarik-gul-6497b21a4/'
+        }
     }
 
     reRoute() {
@@ -68,7 +74,23 @@ class SidebarButtonBox extends React.Component {
                     </div>
                     <div className="github-box">
                         <div className="github-box-inner">
-                            
+                            <div className="github-linkedin-title">
+                                Github / LinkedIn
+                            </div>
+                            <div className="button-container-git-link">
+                                <button onClick={() => this.routePortfolio('github')} className="sidebar-link-button-git">
+                                    <img src="/github-sign.svg" className="sidebar-github-i" />
+                                    <div className="github-text-sidebar">
+                                        Github
+                                    </div>
+                                </button>
+                                <button onClick={() => this.routePortfolio('linkedin')} className="sidebar-link-button-link">
+                                    <img src="/linkedin-logo.svg" className="sidebar-linkedin-i"/>
+                                    <div className="linkedin-text-sidebar">
+                                        LinkedIn
+                                    </div>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
