0003227d6ffc040b4c9de1dc1a7292b78ef159e6
Tarik <tariksnow37@gmail.com>
2020-04-17 19:14:41 -0700

    fix footer

--- vimeo_clone/frontend/components/footer/footer.jsx
+++ vimeo_clone/frontend/components/footer/footer.jsx
@@ -1,32 +1,53 @@
 import React from 'react'
 
-const Footer = props => {
-    return (
-        <div className='footer-container'>
-            <div className='footer-top-container'>
-                <div className='footer-left-container'>
-                    <ul className='list-container-footer'>
-                        <li>
-                            <a className='github-link atag' href='https://github.com/TarikGul'>Github</a>
-                        </li>
-                        <li>
-                            <a className='dreams-tag atag' href='https://www.youtube.com/watch?v=8WsHwXs_aq4'>Dreams</a>
-                        </li>
-                    </ul>
+class Footer extends React.Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    handleReroute(field) {
+        if (field === 'github') {
+            window.location.href = 'https://github.com/TarikGul'
+        } else if (field === 'linkedin') {
+            window.location.href = 'https://www.linkedin.com/in/tarik-gul-6497b21a4/'
+        }
+    }
+
+    render() {
+        return (
+            <div className='footer-container'>
+                <div className='footer-top-container'>
+                    <div className='footer-left-container'>
+                        <ul className='list-container-footer'>
+                            <li>
+                                <img className="github-footer" 
+                                     src="/github-sign.svg"
+                                     onClick={() => this.handleReroute('github')} />
+                                <a className='github-link atag' href='https://github.com/TarikGul'>Github</a>
+                            </li>
+                            <li>
+                                <img className="linked-in-footer" 
+                                     src="/linkedin-logo.svg" 
+                                     onClick={() =>this.handleReroute('linkedin')}/>
+                                <a className='linkedin-link atag' href='https://www.linkedin.com/in/tarik-gul-6497b21a4/'>Linked In</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='footer-right-container'>
+                        
+                    </div>
                 </div>
-                <div className='footer-right-container'>
-                    May the force be with you
-                </div>
-            </div>
-            <div className='footer-bottom-container'>
-                <div className='footer-bottom-inner-container'>
-                    <div className='one-love-sf'>
-                    Made with love in SF
+                <div className='footer-bottom-container'>
+                    <div className='footer-bottom-inner-container'>
+                        <div className='one-love-sf'>
+                        Made with love in SF
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Footer; 
