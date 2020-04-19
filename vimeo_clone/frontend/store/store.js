14602c30548da820154fdbe97397dda65150bc99
Tarik <tariksnow37@gmail.com>
2020-04-19 03:24:48 -0700

    remove logger

--- vimeo_clone/frontend/store/store.js
+++ vimeo_clone/frontend/store/store.js
@@ -4,6 +4,6 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer'
 
 const configureStore = (preloadedState = {}) =>
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger));
+    createStore(rootReducer, preloadedState, applyMiddleware(thunk));
 
 export default configureStore;
