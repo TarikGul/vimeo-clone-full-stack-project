e943fc5d16d031b6d29f83415bffd2c5daac8410
Tarik <tariksnow37@gmail.com>
2020-04-19 02:26:34 -0700

    cleanup

--- vimeo_clone/frontend/entry.jsx
+++ vimeo_clone/frontend/entry.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root'
-import { fetchPost, fetchPosts } from './actions/post_actions'
-// import * as APIUtil from './util/session_api_util'
-import { login } from './actions/session_actions'
-import { fetchUser } from './actions/user_actions'
-
 
 document.addEventListener('DOMContentLoaded', () => {
     let store;
@@ -23,11 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         store = configureStore();
     }   
-    window.fetchUser = fetchUser;
-    window.fetchPost = fetchPost;
-    window.fetchPosts = fetchPosts;
-    window.dispatch = store.dispatch
-    window.getState = store.getState
+
     const root = document.getElementById('root')
     ReactDOM.render(<Root store={store} />, root)
 })
