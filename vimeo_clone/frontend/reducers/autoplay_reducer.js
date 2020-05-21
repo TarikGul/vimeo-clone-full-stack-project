ac6913de5221dc3fdc4efd4f148392065330c918
Tarik <tariksnow37@gmail.com>
2020-05-21 16:00:10 -0700

    autoplay reducer

---
+++ vimeo_clone/frontend/reducers/autoplay_reducer.js
@@ -0,0 +1,14 @@
+import { TOGGLE_AUTOPLAY } from '../actions/toggle';
+
+const autoplayReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign({}, state);
+    switch(action.type) {
+        case TOGGLE_AUTOPLAY:
+            nextState['toggle'] = true
+        default: 
+            return nextState;
+    }
+}
+
+export const autoplayReducer;
