c9100a221440080728530c074973bc37eecb3bd4
Tarik <tariksnow37@gmail.com>
2020-05-21 15:44:54 -0700

    toggle actions

---
+++ vimeo_clone/frontend/actions/toggle.js
@@ -0,0 +1,11 @@
+export const TOGGLE_AUTOPLAY = 'TOGGLE_AUTOPLAY';
+
+const toggleAutoplay = () => {
+    return {
+        type: TOGGLE_AUTOPLAY
+    };
+};
+
+export const dispatchToggle = () => dispatch => {
+    dispatch(toggleAutoplay())
+};
