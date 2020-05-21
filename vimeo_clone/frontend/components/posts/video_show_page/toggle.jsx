e8536d0ff36b3c2c98879562f5d7e8036abecec7
Tarik <tariksnow37@gmail.com>
2020-05-21 15:38:23 -0700

    add check and onchange to autoplay toggle

--- vimeo_clone/frontend/components/posts/video_show_page/toggle.jsx
+++ vimeo_clone/frontend/components/posts/video_show_page/toggle.jsx
@@ -7,13 +7,21 @@ class ToggleButton extends React.Component {
         this.state = {
             clicked: false
         }
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        e.preventDefault();
+
+        console.log('the autoplay was clicked');
     }
 
     render() {
         return (
             <div className="toggle-autplay-container" >
                 <label className="switch">
-                    <input type="checkbox" />
+                    <input type="checkbox" checked={} onChange={this.handleChange}/>
                     <span className="slider round"></span>
                 </label>
                 <div className="autoplay-toggle">
@@ -21,8 +29,7 @@ class ToggleButton extends React.Component {
                 </div>
             </div>
 
-        )
-            
+        )   
     }
 }
 
