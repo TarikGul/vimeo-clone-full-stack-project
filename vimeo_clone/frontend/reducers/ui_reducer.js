c81315e2d543b02e2cca2b6fcebeb43cb81abe5d
Tarik <tariksnow37@gmail.com>
2020-05-21 17:18:48 -0700

    autoplay reducer

--- vimeo_clone/frontend/reducers/ui_reducer.js
+++ vimeo_clone/frontend/reducers/ui_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import modalReducer from './modal_reducer';
 import searchReducer from './search_reducer';
+import autoplayReducer from './autoplay_reducer';
 
 const uiReducer = combineReducers({
         modal: modalReducer,
-        search: searchReducer 
+        search: searchReducer,
+        autoplay: autoplayReducer,
 })
 
 export default uiReducer;
